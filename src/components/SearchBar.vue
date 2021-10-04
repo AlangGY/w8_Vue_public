dc6340a2f5afc1e89ef54d46590f8d43de3c980f
Alang Kim <rlaangh77@naver.com>
2021-10-05 01:22:38 +0900

    - 🎨 style: SearchBar Style2

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,10 +1,16 @@
 <template>
   <form @submit.prevent="searchBy">
-    <input
-      ref="input"
-      :value="searchingBy"
-      type="text"
-      @input="input">
+    <div class="input__container">
+      <span class="material-icons">
+        search
+      </span>
+      <input
+        ref="input"
+        :value="searchingBy"
+        type="text"
+        placeholder="영화명을 입력하세요"
+        @input="input">
+    </div>
   </form>
 </template>
 
@@ -33,22 +39,46 @@ form {
   height: 100%;
   display: flex;
   justify-content: center;
-  input {
+  .input__container {
     width: 50%;
     height: 100%;
-    background-color: color.adjust($color-background, $lightness: 10%);
-    border: none;
-    border-radius: 5px;
-    color: $color-font;
-    font-size: 30px;
-    text-align: center;
+    position: absolute;
+    display: flex;
+    justify-content: center;
     transition: 0.2s ease-in-out;
-    &:focus {
-      outline: none;
+    &:focus-within {
       width: 100%;
-      color: color.adjust($color-background, $blackness: 80%);
-      background-color: color.adjust($color-background, $lightness: 80%);
-      box-shadow: 0 0 5px 0 color.adjust($color-background, $lightness: 50%);
+      .material-icons {
+        color: black;
+      }
+    }
+    .material-icons {
+      position: absolute;
+      font-size: 40px;
+      top: calc(50% - 20px);
+      right: 0;
+      opacity: .7;
+      z-index: 0;
+    }
+    input {
+      width: 100%;
+      height: 100%;
+      background-color: color.adjust($color-background, $lightness: 10%);
+      border: none;
+      border-radius: 5px;
+      color: $color-font;
+      font-size: 30px;
+      text-align: center;
+      transition: 0.2s ease-in-out;
+      &::placeholder {
+        font-size: 20px;
+      }
+      &:focus {
+        outline: none;
+        color: color.adjust($color-background, $blackness: 80%);
+        background-color: color.adjust($color-background, $lightness: 80%);
+        box-shadow: 0 0 5px 0 color.adjust($color-background, $lightness: 50%);
+      }
     }
   }
 }
