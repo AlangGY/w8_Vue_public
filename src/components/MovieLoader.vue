c1997b98e02613b735efc23255d2649d854f5129
Alang Kim <rlaangh77@naver.com>
2021-10-08 02:06:02 +0900

    - 🚜 refactor: MovieLoader 최적화, 불필요 코드 삭제
    기존 setTimeout 활용한 Lazy한 observe 불필요한 작업으로 판단하여 mounted시와 isLoading = false 시에 observe하도록 수정

--- src/components/MovieLoader.vue
+++ src/components/MovieLoader.vue
@@ -1,12 +1,8 @@
 <template>
   <div
+    ref="detectTarget"
     class="movies__loader">
-    <div
-      v-if="showTarget"
-      ref="detectTarget"
-      class="target"></div>
-    <Loading
-      v-else
+    <Loading   
       :size="30" />
   </div>
 </template>
@@ -23,9 +19,6 @@ export default {
     }
   },
   emits : ['touched'],
-  data(){
-    return { showTarget : false };
-  },
   computed : {
     movieLoader(){
       return new IntersectionObserver((entries, observer) => {
@@ -35,49 +28,25 @@ export default {
             observer.unobserve(entry.target);
           }
         });
-      
       }, { threshold : 0 });
     }
   },
   watch : {
     async isLoading(newValue){
-      if (newValue){
-        this.showTarget = false;
-      }
-      else {
-        this.updateShowTargetSlowly(true);
-      }
-    },
-    showTarget(newValue){
-      if (newValue) {
-        this.$nextTick(() => {
-          this.movieLoader.observe(this.$refs.detectTarget);
-        });
+      if(!newValue){
+        this.movieLoader.observe(this.$refs.detectTarget);
       }
     }
   },
   mounted(){
-    this.updateShowTargetSlowly(true);
-    
-  },
-  methods : {
-    updateShowTargetSlowly(bool){
-      setTimeout(() => { 
-        this.showTarget = bool; 
-      }, 1000);
-    }
+    this.movieLoader.observe(this.$refs.detectTarget);
   }
 };
-
 </script>
 
 <style lang="scss" scoped>
 .movies__loader {
   width: 100%;
   height: 100%;
-  .target {
-    height: 5px;
-    width: 100%;
-  }
 }
 </style>
